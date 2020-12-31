9d34ba543a04bd460f4b130286a2cf937dd4d454
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-12-31 06:00:50 -0800

    :recycle: Refactor weathericon component.

--- src/WeatherIcon.tsx
+++ src/WeatherIcon.tsx
@@ -7,18 +7,19 @@ type WeatherIconProps = {
   className?: string;
   name: 'owm' | 'yahoo';
   night?: boolean;
-};
+} & React.ComponentPropsWithRef<'i'>;
 
 const WeatherIcon: React.FC<WeatherIconProps> = ({
   iconId,
   className = '',
   name,
   night = false,
+  ...htmlProps
 }) => {
   const iconClassName = convertCode(name, iconId, night);
   const _className = `${iconClassName} ${className}`;
 
-  return <i className={_className} />;
+  return <i className={_className} {...htmlProps} />;
 };
 
 export default WeatherIcon;
