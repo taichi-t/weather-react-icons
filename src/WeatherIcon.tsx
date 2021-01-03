f4094185976673bea4b24481c72fb2f0dc4ff5dd
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-01-02 17:29:25 -0800

    :recycle: Refactor.

--- src/WeatherIcon.tsx
+++ src/WeatherIcon.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './css/weather-icons.min.css';
+import 'weather-icons/css/weather-icons.css';
 import convertCode from './utils/convertCode';
 
 type WeatherIconProps = (
