7a993c6db5d6ffd5571086ecbe40c1e592b7b508
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-02-10 22:32:31 -0800

    :recycle: Modify example.

--- example/index.tsx
+++ example/index.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+// the following css file must be installed for WeatherIcon component.
+import 'weather-react-icons/lib/css/weather-icons.css';
 import { WeatherIcon } from 'weather-react-icons';
-// if only use css, import css files as follows
-// import 'weather-react-icons/lib/css/weather-icons.css';
-// import 'weather-react-icons/lib/css/weather-icons-wind.css';
 
 ReactDOM.render(
   <WeatherIcon iconId={201} name="owm" />,
